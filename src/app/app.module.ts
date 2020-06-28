8b034da05236a77979b5bd49b564bf9cc366e52e
Kodam, Sheshank <Sheshank.Kodam@echostar.com>
2020-06-28 16:32:45 -0600

    Done with web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AutofocusFixModule } from 'ngx-autofocus-fix'; // <--- new code
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,12 +10,13 @@ import { TodoListComponent } from './components/todo-list/todo-list.component';
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    AutofocusFixModule.forRoot(), // <--- new code
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Todo} from '../../interfaces/todo';
 
 @Component({
   selector: 'todo-list',
@@ -6,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
-  todos: object[];
+  todos: Todo[];
   todoName: string;
   idForTodo: number;
+  beforeEditCache: string;
 
   constructor() { }
 
@@ -22,7 +24,8 @@ export class TodoListComponent implements OnInit {
         name: 'task1',
         completed: true,
         priority: 'low',
-        created_at: '0001-01-01T00:00:00Z'
+        created_at: '0001-01-01T00:00:00Z',
+        editing: false,
       },
       {
         todo_id: 'f4f7f40e-0e76-437c-8158-27b881731d8c',
@@ -30,7 +33,8 @@ export class TodoListComponent implements OnInit {
         name: 'task2',
         completed: false,
         priority: 'high',
-        created_at: '0001-01-01T00:00:00Z'
+        created_at: '0001-01-01T00:00:00Z',
+        editing: false,
       }
     ];
   }
@@ -41,16 +45,39 @@ export class TodoListComponent implements OnInit {
     }
     this.todos.push(
       {
-        todo_id: this.idForTodo,
+        todo_id: this.idForTodo.toString(),
         customer_id: '1100',
         name: this.todoName,
         completed: false,
         priority: 'high',
-        created_at: '0001-01-01T00:00:00Z'
+        created_at: '0001-01-01T00:00:00Z',
+        editing: false,
       }
     );
 
     this.todoName = '';
     this.idForTodo ++;
   }
+
+  editTodo(todo: Todo): void {
+    this.beforeEditCache = todo.name;
+    todo.editing = true;
+  }
+
+  doneEdit(todo: Todo): void {
+    if (todo.name.trim().length === 0) {
+      todo.name = this.beforeEditCache;
+    }
+    todo.editing = false;
+  }
+
+  cancelEdit(todo: Todo): void {
+    todo.name = this.beforeEditCache;
+    todo.editing = false;
+  }
+
+  deleteTodo(todoId: string): void {
+    this.todos = this.todos.filter(todo => todo.todo_id !== todoId);
+  }
 }
+
